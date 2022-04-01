93a0a3a3cd5240ec811cf14b958638bb5d9ef14d
tomixrm <chocolate0785lego@icloud.com>
2022-04-02 02:15:44 +0900

    finish motor libs

--- lib/motor/Motors.cpp
+++ lib/motor/Motors.cpp
@@ -1,37 +1,38 @@
 #include <Motors.h>
 
 Motors::Motors() {
-    pinMode(F45, OUTPUT);
-    pinMode(B45, OUTPUT);
-    pinMode(F135, OUTPUT);
-    pinMode(B135, OUTPUT);
-    pinMode(F225, OUTPUT);
-    pinMode(B225, OUTPUT);
-    pinMode(F315, OUTPUT);
-    pinMode(B315, OUTPUT);
+    pinMode(MOTOR_F45, OUTPUT);
+    pinMode(MOTOR_B45, OUTPUT);
+    pinMode(MOTOR_F135, OUTPUT);
+    pinMode(MOTOR_B135, OUTPUT);
+    pinMode(MOTOR_F225, OUTPUT);
+    pinMode(MOTOR_B225, OUTPUT);
+    pinMode(MOTOR_F315, OUTPUT);
+    pinMode(MOTOR_B315, OUTPUT);
 }
 void Motors::turn(int8_t channel, int16_t power) {
     //チャンネル選択
     switch (channel) {
     case 1:
-        channelMOTOR_F = MOTOR_F45;
-        channelMOTOR_B = MOTOR_B45;
+        channelF = MOTOR_F45;
+        channelB = MOTOR_B45;
         break;
     case 2:
-        channelMOTOR_F = MOTOR_F135;
-        channelMOTOR_B = MOTOR_B135;
+        channelF = MOTOR_F135;
+        channelB = MOTOR_B135;
         break;
     case 3:
-        channelMOTOR_F = MOTOR_F255;
-        channelMOTOR_B = MOTOR_B255;
+        channelF = MOTOR_F225;
+        channelB = MOTOR_B225;
         break;
     case 4:
-        channelMOTOR_F = MOTOR_F315;
-        channelMOTOR_B = MOTOR_B315;
+        channelF = MOTOR_F315;
+        channelB = MOTOR_B315;
         break;
     }
     //最大値設定
-    constrain(power, -255, 255);
+    power = max(power, 255);
+    power = min(power, -255);
     if (power > 0) {
         analogWrite(channelF, abs(power));
         analogWrite(channelB, 0);
@@ -50,10 +51,10 @@ void Motors::run(int16_t p1, int16_t p2, int16_t p3, int16_t p4) {
 
 void Motors::drive(int16_t deg, int16_t Speed, int16_t spin) {
     int16_t p1, p2, p3, p4;
-    p1 = round(sin((movedir - 45) * 3.141592 / 180) * -1 * Speed) + spin;
-    p2 = round(sin((movedir - 135) * 3.141592 / 180) * -1 * Speed) + spin;
-    p3 = round(sin((movedir - 225) * 3.141592 / 180) * Speed) + spin;
-    p4 = round(sin((movedir - 315) * 3.141592 / 180) * Speed) + spin;
+    p1 = round(sin((deg - 45) * 3.141592 / 180) * -1 * Speed) + spin;
+    p2 = round(sin((deg - 135) * 3.141592 / 180) * -1 * Speed) + spin;
+    p3 = round(sin((deg - 225) * 3.141592 / 180) * Speed) + spin;
+    p4 = round(sin((deg - 315) * 3.141592 / 180) * Speed) + spin;
     run(p1, p2, p3, p4);
 }
 
--- lib/motor/Motors.h
+++ lib/motor/Motors.h
@@ -3,21 +3,25 @@
 
 #include "Arduino.h"
 
-#define F45 6
-#define B45 10
-#define F135 7
-#define B135 18
-#define F225 3
-#define B225 4
-#define F315 5
-#define B315 2
+#define MOTOR_F45 6
+#define MOTOR_B45 10
+#define MOTOR_F135 7
+#define MOTOR_B135 18
+#define MOTOR_F225 3
+#define MOTOR_B225 4
+#define MOTOR_F315 5
+#define MOTOR_B315 2
 
 class Motors {
+  public:
     Motors();
     void turn(int8_t channel, int16_t power);
     void run(int16_t p1, int16_t p2, int16_t p3, int16_t p4);
     void drive(int16_t deg, int16_t Speed, int16_t spin);
     void driveVector(int16_t vecX, int16_t vecY, int16_t spin);
+
   private:
+    uint8_t channelF;
+    uint8_t channelB;
 };
 #endif
